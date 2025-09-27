f1d38a9abc8392ac075403cbe361b1f203c0126a
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-09-26 20:08:27 -0500

    Add spacing

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -3,11 +3,16 @@ using Backend.Features.ShortUrls.Create;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<ShortUrlDbContext>(opt => opt.UseInMemoryDatabase("ShortUrl"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
 builder.Services.AddScoped<ICreateShortUrlRepository, CreateShortUrlRepository>();
 
 var app = builder.Build();
+
 app.RegisterCreateShortUrlEndpoint();
+
 app.Run();
