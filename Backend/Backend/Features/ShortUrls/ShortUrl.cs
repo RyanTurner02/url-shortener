5d078e025f97d20a55bd43bff142873f567643f4
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-09-26 20:40:14 -0500

    Use init for CreatedAdd and add comments

--- Backend/Backend/Features/ShortUrls/ShortUrl.cs
+++ Backend/Backend/Features/ShortUrls/ShortUrl.cs
@@ -1,10 +1,28 @@
 ﻿namespace Backend.Features.ShortUrls
 {
+    /// <summary>
+    /// Entity class for storing the mapping between an original URL and its shortened form.
+    /// </summary>
     public class ShortUrl
     {
+        /// <summary>
+        /// Primary key.
+        /// </summary>
         public int Id { get; set; }
+        
+        /// <summary>
+        /// The full original url to be shortened.
+        /// </summary>
         public required string OriginalUrl { get; set; }
+        
+        /// <summary>
+        /// The generated shortened url.
+        /// </summary>
         public required string ShortenedUrl { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        /// <summary>
+        /// UTC timestamp when the entry was created.
+        /// </summary>
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
     }
 }
