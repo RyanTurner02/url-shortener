63c361981899909366738280f9e2caea89cd8d81
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-10-26 21:50:05 -0500

    Create duplicate URL repository class and interface

---
+++ Backend/Url.Shortener/Features/ShortUrls/Create/Repositories/DuplicateUrlRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Url.Shortener.Features.ShortUrls.Create.Repositories
+{
+    /// <summary>
+    /// The duplicate URL repository class.
+    /// </summary>
+    public class DuplicateUrlRepository : IDuplicateUrlRepository
+    {
+        /// <inheritdoc/>
+        public Task<bool> DuplicateOriginalUrl(string originalUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> DuplicateShortUrl(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Url.Shortener/Features/ShortUrls/Create/Repositories/IDuplicateUrlRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Url.Shortener.Features.ShortUrls.Create.Repositories
+{
+    /// <summary>
+    /// The duplicate URL repository interface.
+    /// </summary>
+    public interface IDuplicateUrlRepository
+    {
+        /// <summary>
+        /// Checks if there is a duplicate short URL in the database.
+        /// </summary>
+        /// <param name="shortUrl">The short URL.</param>
+        /// <returns>True if there is a duplicate short URL. False otherwise.</returns>
+        Task<bool> DuplicateShortUrl(string shortUrl);
+
+        /// <summary>
+        /// Checks if there is a duplicate original URL in the database.
+        /// </summary>
+        /// <param name="originalUrl">The original URL.</param>
+        /// <returns>True if there is a duplicate original URL. False otherwise.</returns>
+        Task<bool> DuplicateOriginalUrl(string originalUrl);
+    }
+}
