d1783791f435dc9e0e2f11b2a39bce3c7ded0d42
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-10-26 20:34:18 -0500

    Create url encoder unit test

---
+++ Backend/Url.Shortener.Tests/Features/ShortUrls/Create/Infrastructure/UrlEncoderTests.cs
@@ -0,0 +1,19 @@
+﻿using Url.Shortener.Features.ShortUrls.Create.Infrastructure;
+
+namespace Url.Shortener.Tests.Features.ShortUrls.Create.Infrastructure
+{
+    public sealed class UrlEncoderTests
+    {
+        [Fact]
+        public void Encode_ReturnsEncodedUrl()
+        {
+            var hash = "0f115db062b7c0dd030b16878c99dea5c354b49dc37b38eb8846179c7783e9d7";
+            var expected = "BEiD8U92IoYW2ofPhe7GyEBLStEfKccm3G75JyiilanAt8FcsIwpKYWhB2HAZ8CMpN8gZlo5XXHPlYG0HyRslr";
+
+            var urlEncoder = new UrlEncoder();
+            var actual = urlEncoder.Encode(hash);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
