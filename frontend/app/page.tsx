47d9f0fcebfe8572e4dc5be0a7efc672e6e28339
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-11-06 19:52:51 -0600

    Update url shortener form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,24 +4,26 @@ import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        <h1 className="text-4xl font-bold text-center">URL Shortener</h1>
-        <FieldSet>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="url">Paste your long link here</FieldLabel>
-              <Input id="url" type="text" placeholder="https://example.com/long-url-here" />
-            </Field>
-          </FieldGroup>
-          <Button className="cursor-pointer" variant="outline">Submit</Button>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="shortened-url">Shortened URL</FieldLabel>
-              <Input className="text-red-500" id="shortened-url" type="text" placeholder="https://url-shortener-example.com/shortened-url" readOnly />
-            </Field>
-          </FieldGroup>
-        </FieldSet>
+    <div className="h-screen flex justify-center ">
+      <main className="w-full max-w-3xl">
+        <h1 className="p-10 text-4xl font-bold text-center">URL Shortener</h1>
+        <div className="p-10">
+          <FieldSet>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="url">Paste your long link here</FieldLabel>
+                <Input id="url" type="text" placeholder="https://example.com/long-url-here" />
+              </Field>
+            </FieldGroup>
+            <Button className="cursor-pointer" variant="outline">Submit</Button>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="shortened-url">Shortened URL</FieldLabel>
+                <Input className="text-red-500" id="shortened-url" type="text" placeholder="https://url-shortener-example.com/shortened-url" readOnly />
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+        </div>
       </main>
     </div>
   );
