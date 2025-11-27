acc3623dde97f624e88f53f35f1abda338dccb8f
Ryan Turner <ryanturner02@protonmail.com>
2025-11-27 18:42:59 -0500

    Create field error test

--- frontend/components/forms/url-shortener-form/__tests__/url-shortener-form.test.tsx
+++ frontend/components/forms/url-shortener-form/__tests__/url-shortener-form.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import { UrlShortenerForm } from "../url-shortener-form";
+import { UrlShortenerForm } from "@/components/forms/url-shortener-form/url-shortener-form";
 import { QueryProviderWrapper } from "@/test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("UrlShortenerForm", () => {
   it("renders the fields and submit button", () => {
@@ -16,5 +17,21 @@ describe("UrlShortenerForm", () => {
     expect(button).toBeInTheDocument();
     expect(originalUrlField).toBeInTheDocument();
     expect(shortUrlField).toBeInTheDocument();
+  });
+
+  it("displays a field error when submitting an invalid original url", async () => {
+    const user = userEvent.setup();
+    render(
+        <QueryProviderWrapper>
+            <UrlShortenerForm />
+        </QueryProviderWrapper>
+    );
+
+    const button = screen.getByRole("button");
+    await user.click(button);    
+    
+    const error = screen.getByRole("alert");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveTextContent("Invalid URL");
   });
 });
