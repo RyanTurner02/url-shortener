80fd135f875f4902601620c92910b3ea6d821873
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-12-06 19:43:56 -0600

    Keep track of previous original URL

--- frontend/hooks/use-url-shortener-form.tsx
+++ frontend/hooks/use-url-shortener-form.tsx
@@ -7,16 +7,23 @@ import {
 import { IShortUrlResponse } from "@/responses/short-url-response";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
 
 export default function useUrlShortenerForm() {
   const form = useForm<UrlShortenerFormValues>({
     resolver: zodResolver(urlShortenerFormSchema),
     defaultValues: defaultUrlShortenerFormValues,
   });
-
+  const previousOriginalUrl = useRef("");
   const createShortUrlMutation = useCreateShortenedUrl();
 
   const onSubmit = async (data: UrlShortenerFormValues) => {
+    if (data.originalUrl === previousOriginalUrl.current) {
+      return;
+    }
+
+    previousOriginalUrl.current = data.originalUrl;
+    
     const result: IShortUrlResponse = await createShortUrlMutation(
       data.originalUrl
     );
