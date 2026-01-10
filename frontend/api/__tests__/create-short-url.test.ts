dd1de37bc1e255ab7f51f7d55db2dcb6e3514b6e
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2026-01-10 14:01:36 -0600

    Create passing DuplicatedConflictResponse test

--- frontend/api/__tests__/create-short-url.test.ts
+++ frontend/api/__tests__/create-short-url.test.ts
@@ -2,33 +2,58 @@ import { createShortUrl } from "@/api/create-short-url";
 import { DuplicateConflictResponse } from "@/responses/duplicate-conflict-response";
 import { NullShortUrlResponse } from "@/responses/null-short-url-response";
 import { ShortUrlResponse } from "@/responses/short-url-response";
+import { AxiosError, AxiosHeaders } from "axios";
+import axios from "axios";
 
 describe("createShortUrl", () => {
     beforeEach(() => {
-        vi.clearAllMocks();
+        vi.restoreAllMocks();
     });
 
     it("creates a short URL", async () => {
         const expected = new ShortUrlResponse("ShortUrl");
-        const response = await createShortUrl("OriginalUrl");
+        const actual = await createShortUrl("OriginalUrl");
 
-        expect(response).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 
     it("returns DuplicatedConflictResponse on duplicate", async () => {
+        const mockRequest = {};
+        const mockConfig = { headers: new AxiosHeaders() };
+        const mockResponse = {
+            status: 409,
+            statusText: "Conflict",
+            data: {
+                error: "DuplicateConflict",
+                message: "Failed to generate a unique short URL. Please try again later."
+            },
+            headers: new AxiosHeaders(),
+            config: mockConfig,
+            request: mockRequest
+        };
+        const axiosError = new AxiosError(
+            "Request failed with status code 404",
+            "ERR_BAD_REQUEST",
+            mockConfig,
+            mockRequest,
+            mockResponse
+        );
+        vi.spyOn(axios, "post")
+            .mockRejectedValue(axiosError);
+
         const expected = new DuplicateConflictResponse(
             "DuplicateConflict",
             "Failed to generate a unique short URL. Please try again later."
         );
-        const response = await createShortUrl("duplicate");
+        const actual = await createShortUrl("duplicate");
 
-        expect(response).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 
     it("returns NullShortUrlResponse on failure", async () => {
         const expected = new NullShortUrlResponse();
-        const response = await createShortUrl("error");
-        
-        expect(response).toEqual(expected);
+        const actual = await createShortUrl("error");
+
+        expect(actual).toEqual(expected);
     });
 });
