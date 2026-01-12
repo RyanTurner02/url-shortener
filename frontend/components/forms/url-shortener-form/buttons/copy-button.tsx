da9d97c86077441e0c7b823be69f630ee29bfe2b
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2026-01-11 23:32:03 -0600

    Update button type

--- frontend/components/forms/url-shortener-form/buttons/copy-button.tsx
+++ frontend/components/forms/url-shortener-form/buttons/copy-button.tsx
@@ -3,7 +3,7 @@ import { CopyIcon } from "lucide-react";
 
 export default function CopyButton() {
   return (
-    <Button className="cursor-pointer">
+    <Button className="cursor-pointer" type="button">
       Copy <CopyIcon />
     </Button>
   );
