3f40c749c511f8892c2ea43d991153cc0ec4375b
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2026-01-31 11:18:46 -0600

    Update the create short URL endpoint unit tests

--- Backend/Url.Shortener.Tests/Features/ShortUrls/Create/CreateShortUrlEndpointTests.cs
+++ Backend/Url.Shortener.Tests/Features/ShortUrls/Create/CreateShortUrlEndpointTests.cs
@@ -8,8 +8,20 @@ using Url.Shortener.Features.ShortUrls.Create;
 
 namespace Url.Shortener.Tests.Features.ShortUrls.Create
 {
+    /// <summary>
+    /// Tests the <see cref="CreateShortUrlEndpoint"/> class.
+    /// </summary>
     public sealed class CreateShortUrlEndpointTests
     {
+        /// <summary>
+        /// The endpoint route.
+        /// </summary>
+        private const string ROUTE = "/";
+
+        /// <summary>
+        /// Tests creating a short URL with an invalid URL.
+        /// </summary>
+        /// <param name="url">The invalid URL.</param>
         [Theory]
         [InlineData("")]
         [InlineData(" ")]
@@ -18,13 +30,11 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
         [InlineData("www.example.com")]
         public async Task CreateShortUrl_InvalidUrl_ReturnsBadRequest(string url)
         {
-            var route = "/";
-            var payload = new { url = url };
-
             await using var app = new WebApplicationFactory<Program>();
             using var client = app.CreateClient();
 
-            var response = await client.PostAsJsonAsync(route, payload);
+            var payload = new { url = url };
+            var response = await client.PostAsJsonAsync(ROUTE, payload);
             var body = await response.Content.ReadFromJsonAsync<InvalidUrlResponse>();
 
             Assert.Equal(StatusCodes.Status400BadRequest, (int)response.StatusCode);
@@ -34,13 +44,14 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
             Assert.Equal("URL must be valid and not exceed 128 characters.", body.Message);
         }
 
+        /// <summary>
+        /// Tests that an empty generated short URL results in a conflict response.
+        /// </summary>
         [Fact]
-        public async Task CreateShortUrl_ReturnsProblem()
+        public async Task CreateShortUrl_EmptyShortUrl_ReturnsConflict()
         {
-            var originalUrl = "https://example.com/";
             var hash = string.Empty;
-            var route = "/";
-            var payload = new { url = originalUrl };
+            var originalUrl = "https://example.com/";
 
             var senderMock = new Mock<ISender>();
             senderMock.Setup(s =>
@@ -49,21 +60,11 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
                     It.IsAny<CancellationToken>()))
                 .ReturnsAsync(hash);
 
-            await using var app =
-                new WebApplicationFactory<Program>()
-                    .WithWebHostBuilder(builder =>
-                    {
-                        builder.ConfigureServices(services =>
-                        {
-                            var toRemove = services.Where(d => d.ServiceType == typeof(ISender)).ToList();
-                            foreach (var d in toRemove) services.Remove(d);
-                            services.AddSingleton(senderMock.Object);
-                        });
-                    });
-
+            await using var app = CreateApp(senderMock.Object);
             using var client = app.CreateClient();
 
-            var response = await client.PostAsJsonAsync(route, payload);
+            var payload = new { url = originalUrl };
+            var response = await client.PostAsJsonAsync(ROUTE, payload);
             var body = await response.Content.ReadFromJsonAsync<DuplicateConflictResponse>();
 
             Assert.Equal(StatusCodes.Status409Conflict, (int)response.StatusCode);
@@ -80,12 +81,15 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
                 Times.Once);
         }
 
+        /// <summary>
+        /// Tests that creating a valid URL results in a created response with the shortened URL.
+        /// </summary>
         [Fact]
-        public async Task CreateShortUrl_ReturnsCreated()
+        public async Task CreateShortUrl_ValidUrl_ReturnsCreated()
         {
-            var originalUrl = "https://example.com/";
-            var hash = "LliXArW";
+            var hash = "ShortUrl";
             var shortenedUrl = $"http://localhost/{hash}";
+            var originalUrl = "https://example.com/";
 
             var senderMock = new Mock<ISender>();
             senderMock.Setup(s =>
@@ -94,31 +98,17 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
                     It.IsAny<CancellationToken>()))
                 .ReturnsAsync(hash);
 
-            await using var app =
-                new WebApplicationFactory<Program>()
-                    .WithWebHostBuilder(builder =>
-                    {
-                        builder.ConfigureServices(services =>
-                        {
-                            var toRemove = services.Where(d => d.ServiceType == typeof(ISender)).ToList();
-                            foreach (var d in toRemove) services.Remove(d);
-
-                            services.AddSingleton(senderMock.Object);
-                        });
-                    });
-
+            await using var app = CreateApp(senderMock.Object);
             using var client = app.CreateClient();
 
-            var route = "/";
             var payload = new { url = originalUrl };
-            var response = await client.PostAsJsonAsync(route, payload);
+            var response = await client.PostAsJsonAsync(ROUTE, payload);
 
             var expectedBody = new ShortUrlResponse(shortenedUrl);
             var body = await response.Content.ReadFromJsonAsync<ShortUrlResponse>();
 
             Assert.True(response.IsSuccessStatusCode);
             Assert.Equal(expectedBody, body);
-            Assert.Equal(route, response.Headers.Location?.OriginalString);
 
             senderMock.Verify(
                 s => s.Send(
@@ -126,6 +116,26 @@ namespace Url.Shortener.Tests.Features.ShortUrls.Create
                         c => c.Url == originalUrl),
                     It.IsAny<CancellationToken>()),
                 Times.Once);
+        }
+
+        /// <summary>
+        /// Creates a web application used for testing.
+        /// </summary>
+        /// <param name="senderMock">The ISender mock object.</param>
+        /// <returns>The web application used for testing.</returns>
+        private WebApplicationFactory<Program> CreateApp(ISender senderMock)
+        {
+            return new WebApplicationFactory<Program>()
+                    .WithWebHostBuilder(builder =>
+                    {
+                        builder.ConfigureServices(services =>
+                        {
+                            var toRemove = services.Where(d => d.ServiceType == typeof(ISender)).ToList();
+                            foreach (var d in toRemove) services.Remove(d);
+
+                            services.AddSingleton(senderMock);
+                        });
+                    });
         }
     }
 }
